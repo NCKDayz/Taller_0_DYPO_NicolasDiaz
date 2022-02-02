091508798d9f5438166d6a1b5a2efcbc44320e01
Nicolas Diaz Montana <n.diaz9@uniandes.edu.co>
2022-02-01 20:30:23 -0500

    Listo para Entrega

---
+++ Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,28 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modificacion 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hola, mundo!!!");
+		CalculadoraEstadisticas calc = null;
+		try {
+			calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR: el archivo indicado no se encontró.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("ERROR: hubo un problema leyendo el archivo.");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		System.out.println(calc.paisConMasMedallistas());
+	}
+	
+}
